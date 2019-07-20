d4b1546457b36933d9cc4a392f78c955004224f2
Ibidapo Rasheed <dapsined@gmail.com>
2019-07-20 22:47:44 +0100

    Implement realtime messaging

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,12 +4,16 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to root_path
+      ActionCable.server.broadcast "chatroom_channel", message: render_message(message)
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def render_message(message)
+    render partial: 'message', locals: { message: message }
   end
 end
