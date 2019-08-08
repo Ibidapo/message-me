34a197ab90ecaaab388eb1969e1a78baec2b089b
Ibidapo Rasheed <dapsined@gmail.com>
2019-08-08 23:49:12 +0100

    Test User show controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
+require_relative '../helpers/sessions_helper.rb'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'POST #create'do
-    let(:user) { create(:user) }
+  let(:user) { create(:user) }
 
+  describe 'GET #new' do
+    context 'when url is loaded' do
+      it 'should load the new template' do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'POST #create'do
     context 'when credentials are valid' do
       it 'should flash a success message and set session id' do
         post :create, params: { user: { username: "new_user", password: "default_pass" } }
@@ -28,6 +38,21 @@ RSpec.describe UsersController, type: :controller do
         
         expect(flash[:error]).to eq "Invalid credentials"
         expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    include SessionsHelper
+
+    context 'when url is loaded' do
+      before do
+        login(user) 
+      end
+      
+      it 'should render the show template' do
+        get :show, params: { id: user.id }
+        expect(response).to render_template :show
       end
     end
   end
